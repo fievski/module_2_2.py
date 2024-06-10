48bd1843722bc752e93a21258ac76149a41cd15e
fievski <fievski@yandex.ru>
2024-06-10 23:24:00 +0600

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,40 @@
+first = 656          # Условная конструкция. Операторы if, elif, else."
+second = 777
+third = 989
+
+if first == second and third :
+    print(3)
+if second == third :
+    print(2)
+if not first == second and third :
+    print(0)
+
+first = 69
+second = 79
+third = 69
+
+if first == second and third :
+    print(3)
+if first == third :
+    print(2)
+if first == second and third :
+    print(0)
+
+
+# number = int(input('Введите число: '))
+# if number % 3 == 0 and number % 5 == 0:
+    # print('Kiss')
+# elif number % 3 == 0:
+    # print('Ass')
+# elif number % 5 == 0:
+    # print('Pass')
+# else :
+    # print('Число не робит')
+
+# name = input("Введите Ваше имя: ")
+# if name == 'Urban' :
+    # print('Здравствуйте, администратор')
+# if name == 'Сёма' :
+    # print('Здарова, бобёр')
+# else:
+    # print("Привет!", name)
